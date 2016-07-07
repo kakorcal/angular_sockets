5aac8fcd06b3a9c08fb95735e768b8a53f0f781d
Kenneth Korcal <kakorcal@gmail.com>
2016-07-07 10:23:23 -0700

    chat layout complete

--- client/javascripts/components/chat.controller.js
+++ client/javascripts/components/chat.controller.js
@@ -3,8 +3,42 @@
     .controller('ChatController', ChatController);
 
   function ChatController($scope, ChatService){
+    let vm = this;
+
+    vm.chats = [
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'},
+      {user: 'Ken', text: 'Hello'},
+      {user: 'Foo', text: 'Bar baz'},
+      {user: 'Test', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus aspernatur, blanditiis laboriosam, animi necessitatibus sapiente.'}
+    ];
+
     // when user goes on chat.html, open sockets
     ChatService.connect();
+
+    // 
 
     // when user leaves, disconnect sockets
     $scope.$on('$locationChangeStart', e=>{
